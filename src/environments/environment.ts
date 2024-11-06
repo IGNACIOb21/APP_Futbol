d025179d0e4b57cf5eeb2f1b9de3317fb6dfb55c
IGNACIOb21 <166768975+IGNACIOb21@users.noreply.github.com>
2024-11-06 14:40:36 -0300

    NUEVA VASE DE DATOS

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyCJ3YqMfQBp_QG7ul8e0d2PH3UB3UKIQJQ",
-    authDomain: "app-futbol-base.firebaseapp.com",
-    projectId: "app-futbol-base",
-    storageBucket: "app-futbol-base.appspot.com",
-    messagingSenderId: "210395209272",
-    appId: "1:210395209272:web:0c502b770c3ea70b5510cd",
-    measurementId: "G-KQ12FM5WE4"
+    apiKey: "AIzaSyAKwjqHfdqSZu1uKkeDtA6a2s5M9zAQZaw",
+    authDomain: "app-futbol-faa46.firebaseapp.com",
+    projectId: "app-futbol-faa46",
+    storageBucket: "app-futbol-faa46.firebasestorage.app",
+    messagingSenderId: "141574406855",
+    appId: "1:141574406855:web:19c1a3b139165c874174e2",
+    measurementId: "G-6MHMY3TB2D"
   }
 };
 
